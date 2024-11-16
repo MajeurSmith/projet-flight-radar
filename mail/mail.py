e8234e11a56852be0aa70596f2db6125ee04b5f1
Axel Chavand <141035728+MajeurSmith@users.noreply.github.com>
2024-11-16 13:12:05 +0100

    Update mail.py

--- mail/mail.py
+++ mail/mail.py
@@ -3,13 +3,9 @@ import win32com.client as win32
 
 def envoieMail(txt : str, objet : str, destinataire : str):
     outlook = win32.Dispatch('outlook.application')
-    namespace = outlook.GetNamespace('MAPI')
-    account = namespace.Folders.Item("axel.chavand@ipsa.fr")
-    email.SendUsingAccount = account
     email = outlook.CreateItem(0)
     email.to = destinataire
     email.Subject = objet
     email.HTMLBody=txt
-
-
-envoieMail("""<p>Ceci est un mail</p>""","TEST","axel.chavand@ipsa.fr")
+    email.Send()
+    print("mail envoyé")
