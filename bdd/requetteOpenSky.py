1b64da9b7019b80b42075754abaf94dfcdc5e645
Axel Chavand <141035728+MajeurSmith@users.noreply.github.com>
2024-12-12 17:21:38 +0100

    update ui

--- bdd/requetteOpenSky.py
+++ bdd/requetteOpenSky.py
@@ -1,20 +1,30 @@
 import requests
 from requests.auth import HTTPBasicAuth
+from datetime import datetime, timedelta
 
 # Définir les informations d'identification
 username = 'your_username'
 password = 'your_password'
-
+ICAO = "LFPG"
+end_time = datetime.utcnow()
+start_time = end_time - timedelta(hours=1)
+start_timestamp = int(start_time.timestamp())
+end_timestamp = int(end_time.timestamp())
 # URL de l'API OpenSky
-url = 'https://opensky-network.org/api/states/all'
+
+urlGeneral="https://opensky-network.org/api/flights/arrival?airport={ICAO}&begin={start_timestamp}&end={end_timestamp}"
+url = "https://opensky-network.org/api/flights/arrival?airport=LFPG&begin=1702219200&end=1702222800"
 
 # Faire une requête GET avec les identifiants pour récupérer les données
 response = requests.get(url, auth=HTTPBasicAuth("majeurSmith", "9f196160"))
 
 print("tarte au citron")
-data = response.json()
-print("chocolat")
-print(data)
+if response.status_code==200:
+   data = response.json()
+   print(data)
+else : print(f"Erreur: {response.status_code}")
+
+"""
 for flight in data['states']:
    print(f"Flight {flight[1]} is at latitude {flight[6]} and longitude {flight[5]}")
-
+"""
--- pages/test 1.py
+++ pages/test 1.py
@@ -1,5 +1,6 @@
 from PyQt5 import uic, QtWidgets
 
+#from Couleur import jaune
 
 win0 = ""
 win1 = ""
@@ -7,11 +8,14 @@ win2 = ""
 win3 = ""
 win4 = ""
 win5 = ""
+win6 = ""
+win7 = ""
 app =  ""
 
 
 def connexion():   #Ouvre la page de connexion
     global win1
+    win1.close()
 
     win1.Id1.setPlaceholderText("Identifiant")
     win1.MDP1.setPlaceholderText("Mot de passe")
@@ -23,6 +27,7 @@ def connexion():   #Ouvre la page de connexion
 
 def creation():    #Ouvre la page de création de compte
     global win2
+    win2.close()
 
     win2.Id2.setPlaceholderText("Identifiant")
     win2.MDP2.setPlaceholderText("Mot de passe")
@@ -37,6 +42,7 @@ def creation():    #Ouvre la page de création de compte
 
 def suppression():    #Ouvre la page de suppression de compte
     global win3
+    win3.close()
 
     win3.Id3.setPlaceholderText("Identifiant")
     win3.MDP3.setPlaceholderText("Mot de passe")
@@ -51,6 +57,7 @@ def suppression():    #Ouvre la page de suppression de compte
 
 def aeroport():    #Ouvre la page d'interface des aéroports
     global win4 , app
+    win4.close()
 
     ville = app.sender()                                                       #Place la bonne ville dans le titre
     win4.Titre4.setText(f"Liste des vols opérant depuis {ville.objectName()}")
@@ -71,13 +78,14 @@ def aeroport():    #Ouvre la page d'interface des aéroports
         win4.Liste4_2.addItem(str(numéro_vol2[num]))
 
 
-    win4.Liste4_1.itemClicked.connect(vol)
-    win4.Liste4_2.itemClicked.connect(vol)
+    win4.Liste4_1.itemClicked.connect(vol_départ)
+    win4.Liste4_2.itemClicked.connect(vol_arrivé)
 
 
     win4.Retour4.clicked.connect(fin_win4)
     win4.show()
 
+"""
 def vol(item):
     global win5
     print("vol")
@@ -105,8 +113,51 @@ def vol(item):
 
     win5.OK5.clicked.connect(fin_win5)
     win5.show()
+"""
 
+def vol_arrivé():
+    global win6
+    win6.close()
 
+    compagnie = "airfrace"
+    avion = "a380"
+    piste = "5"
+    terminal = "3"
+    porte = "22"
+    heure = "18H15"
+    retard = "15min"
+
+    win6.tableWidget.setItem(0 , 0 , QtWidgets.QTableWidgetItem(compagnie))
+    win6.tableWidget.setItem(1 , 0 , QtWidgets.QTableWidgetItem(avion))
+    win6.tableWidget.setItem(2 , 0 , QtWidgets.QTableWidgetItem(piste))
+    win6.tableWidget.setItem(3 , 0 , QtWidgets.QTableWidgetItem(terminal))
+    win6.tableWidget.setItem(4 , 0 , QtWidgets.QTableWidgetItem(porte))
+    win6.tableWidget.setItem(5 , 0 , QtWidgets.QTableWidgetItem(heure))
+    win6.tableWidget.setItem(6 , 0 , QtWidgets.QTableWidgetItem(retard))
+
+    win6.show()
+
+def vol_départ():
+    global win7
+    win7.close()
+
+    compagnie = "airfrace"
+    avion = "a380"
+    piste = "5"
+    terminal = "3"
+    porte = "22"
+    heure = "18H15"
+    retard = "15min"
+
+    win7.tableWidget.setItem(0 , 0 , QtWidgets.QTableWidgetItem(compagnie))
+    win7.tableWidget.setItem(1 , 0 , QtWidgets.QTableWidgetItem(avion))
+    win7.tableWidget.setItem(2 , 0 , QtWidgets.QTableWidgetItem(piste))
+    win7.tableWidget.setItem(3 , 0 , QtWidgets.QTableWidgetItem(terminal))
+    win7.tableWidget.setItem(4 , 0 , QtWidgets.QTableWidgetItem(porte))
+    win7.tableWidget.setItem(5 , 0 , QtWidgets.QTableWidgetItem(heure))
+    win7.tableWidget.setItem(6 , 0 , QtWidgets.QTableWidgetItem(retard))
+
+    win7.show()
 
 
 def fin_win1():
@@ -125,16 +176,25 @@ def fin_win4():
 
 def fin_win5():
     win5.close()
+    win6.close()
+    win7.close()
 
 
 
 
 def fin_programme():
     win0.close()
+    win1.close()
+    win2.close()
+    win3.close()
+    win4.close()
+    win5.close()
+    win6.close()
+    win7.close()
     sys.exit()
 
 def debut_programme():
-    global win0 , win1 , win2 , win3 , win4 , win5 , app
+    global win0 , win1 , win2 , win3 , win4 , win5 , win6 , win7 , app
 
     app = QtWidgets.QApplication([])
     win0 = uic.loadUi("ui/Carte-V3.ui") # fenêtre principal
@@ -185,6 +245,8 @@ def debut_programme():
     win3 = uic.loadUi("ui/Suppression de compte.ui")  # page de suppression de compte
     win4 = uic.loadUi("ui/Interface aéroport.ui") # fenêtre aéroport
     win5 = uic.loadUi("ui/Infos vols.ui") # page des vols
+    win6 = uic.loadUi("ui/Infos vols V2.ui")
+    win7 = uic.loadUi("ui/Infos vols départ.ui")
 
     win0.show()
     app.exec()
